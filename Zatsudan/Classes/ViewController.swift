f972ca5caeae383733ccccad48fd111ee461565a
TakayukiHARA <takayuki.hara@hispot.co.jp>
2016-05-29 09:07:39 +0900

    シングルビュープロジェクトの追加

---
+++ Zatsudan/Classes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Zatsudan
+//
+//  Created by TakayukiHARA on 2016/05/29.
+//  Copyright © 2016年 Hispot Co., LTD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
